8d742a548401eb9613e18725c33b926e711cfb1e
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2021-05-08 11:30:59 +0300

    Clear dependencies. Move to sentry-go. Some refactoring, lints, tests.

--- fasthttp_test.go
+++ fasthttp_test.go
@@ -1,16 +1,19 @@
-package log
+package log_test
 
 import (
 	"bytes"
 	"testing"
 
+	log "github.com/spacetab-io/logs-go/v2"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFHLogger_Printf(t *testing.T) {
+	t.Parallel()
+
 	out := &bytes.Buffer{}
 	_ = initLog(out)
-	fhl := FHLogger{}
+	fhl := log.FHLogger{}
 	fhl.Printf("some %s", "data")
 
 	exp := "DBG |> some data <|"
---
+++ logger_internal_test.go
@@ -0,0 +1,61 @@
+package log
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_contextFields(t *testing.T) {
+	type tc struct {
+		name string
+		in   context.Context
+		exp  map[string]interface{}
+	}
+
+	rID := newTestUUID("45bf025d-9e46-45a4-8562-c37c4d48a9ca")
+
+	tcs := []tc{
+		{
+			name: "context with requers_id",
+			in:   context.WithValue(context.WithValue(context.Background(), "environment", "test"), ctxRequestIDKey, rID),
+			exp:  map[string]interface{}{ctxRequestIDKey: rID.String()},
+		},
+		{
+			name: "empty context",
+			in:   context.Background(),
+			exp:  map[string]interface{}{},
+		},
+		{
+			name: "string equest id",
+			in:   context.WithValue(context.WithValue(context.Background(), "environment", "test"), ctxRequestIDKey, rID.String()),
+			exp:  map[string]interface{}{ctxRequestIDKey: rID.String()},
+		},
+	}
+
+	t.Parallel()
+
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			out := contextFields(tc.in)
+
+			assert.Equal(t, tc.exp, out)
+		})
+	}
+}
+
+type testUUID struct {
+	str string
+}
+
+func (t testUUID) String() string {
+	return t.str
+}
+
+func newTestUUID(str string) testUUID {
+	return testUUID{str: str}
+}
